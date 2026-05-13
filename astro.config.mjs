473aa364e013db0c97a0586fa2f47f84e6531867
Sasha <147207306+AlexandraB-C@users.noreply.github.com>
2026-05-13 13:01:12 +0300

    Deploy forms (#48)
    * Fix pages
    
    * Fix forms
    
    * Add matomo
    
    * Update docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://tum-faf.github.io',
+  base: '/FAF-Site-2.0',
+  trailingSlash: 'ignore',
+});
